e45f4e98945cb49771db5943e5b5d856f2067a14
thiagosena <lord.sena@gmail.com>
2014-06-05 14:25:15 -0300

    auto testing in travis ci #3

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,16 +1,29 @@
 import org.junit.*;
+
+import controllers.Application;
 import play.test.*;
 import play.mvc.*;
 import play.mvc.Http.*;
 
 public class ApplicationTest extends FunctionalTest {
 
+	@Test
+    public void simpleCheck() {
+        int a = 1 + 1;
+        assertEquals(a, 2);
+    }
+
     @Test
-    public void testThatIndexPageWorks() {
-        Response response = GET("/");
-        assertIsOk(response);
-        assertContentType("text/html", response);
-        assertCharset(play.Play.defaultWebEncoding, response);
+    public void renderParseSumUrl() {
+        Application teste = new Application();
+        String result = teste.parse("http://calclib-thiagosena.rhcloud.com/calclib/sum?valor1=10&valor2=20");
+        assertEquals(result, "30.0");
     }
     
+    @Test
+    public void renderParseMinusUrl() {
+        Application teste = new Application();
+        String result = teste.parse("http://calclib-thiagosena.rhcloud.com/calclib/minus?valor1=20&valor2=10");
+        assertEquals(result, "10.0");
+    }
 }
